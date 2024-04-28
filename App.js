62084441365f90595df9658e3e3d2ed901a11e93
Matias Garafoni <matias.garafoni@gmail.com>
2024-04-28 01:28:29 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Te Amo Gorda! Con toda mi alma mi tesoro y te voy a extrañar!</Text>
       <StatusBar style="auto" />
     </View>
   );
