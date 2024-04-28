ce4863b9e19f0933ce57a617e8fe3cf6ff21b0ed
Matias Garafoni <matias.garafoni@gmail.com>
2024-04-28 17:48:53 -0300

    misc

--- App.js
+++ App.js
@@ -1,36 +1,48 @@
-import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable } from "react-native";
+import { useState } from "react";
+import { View, Alert, ActivityIndicator, Text, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar} from "react-native";
+import Greet from "./components/greet";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isLoaderVisible, setIsLoaderVisible] = useState(false);
   return (
   <View style={{ flex:1, backgroundColor: "plum", padding: 60}}>
+    <Greet name="Bruce Wayne" />
+    <Greet name="Clark Kent" />
+    <Button title="Alert" onPress={() => Alert.alert("Invalid Data","Data was incorrect")}></Button>
+        <ActivityIndicator size="large" color="midnightblue" animating={isLoaderVisible} />
+    <Button title="Alert 2" onPress={() => Alert.alert("Invalid Data","Data was incorrect", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel pressed")
+      },
+      {
+        text : "OK",
+        onPress: () => console.log("OK Pressed")
+      }
+    ])}></Button>
+    
+        <ActivityIndicator size="large" color="midnightblue" animating={isLoaderVisible} />
+    <StatusBar
+      backgroundColor="lightgreen" barStyle="light-content" hidden
+    />
     <Button
       title='Press'
-      onPress={()=> console.log('Button Pressed')}
+      onPress={()=> setIsModalVisible(true)}
       color="midnightblue"
     />
-    <Pressable onPress={() => console.log("Image Pressed")}>
-      <Image source={logoImg} style={{ width:300, height:300}}/>
-    </Pressable>
-    <Pressable onLongPress={() => console.log("Text Pressed")}>
-      <Text>
-        asdasdsaassd
-        asdasdsaassdas
-        defaultada
-        a
-        sds
-        
-        adaptivedsa
-        sdsdad
-        asdasdsaassda
-        
-        asdasdsaassdad
-        adaptivedsadsad
-        adaptive34
-        4
-        3
-      </Text>
-    </Pressable>
+    <Modal 
+      visible={isModalVisible}
+      onRequestClose={() => setIsModalVisible(false)}
+      animationType="slide"
+      presentationStyle="pageSheet"
+    >
+      <View style={{ flex:1, backgroundColor: "lightblue", padding: 60}}>
+        <Text>Modal Content</Text>
+        <Button title="Close" color="midnightblule" onPress={() => setIsModalVisible(false)}/>
+      </View>
+    </Modal>
   </View>
   );
 }
