a5e9fb3b7ff6c65ab2e56788eba755559c70b67d
Matias Garafoni <matias.garafoni@gmail.com>
2024-04-28 02:49:06 -0300

    misc

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Te Amo Gorda! Con toda mi alma mi tesoro y te voy a extrañar!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={{ flex:1, backgroundColor: "plum", padding: 60}}>
+    <Button
+      title='Press'
+      onPress={()=> console.log('Button Pressed')}
+      color="midnightblue"
+    />
+    <Pressable onPress={() => console.log("Image Pressed")}>
+      <Image source={logoImg} style={{ width:300, height:300}}/>
+    </Pressable>
+    <Pressable onLongPress={() => console.log("Text Pressed")}>
+      <Text>
+        asdasdsaassd
+        asdasdsaassdas
+        defaultada
+        a
+        sds
+        
+        adaptivedsa
+        sdsdad
+        asdasdsaassda
+        
+        asdasdsaassdad
+        adaptivedsadsad
+        adaptive34
+        4
+        3
+      </Text>
+    </Pressable>
+  </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
